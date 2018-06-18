8df94e7104b1c442682a03571e5f8927b97d63e6
Jeff Bupp <jbupp@DOMAIN.local>
2018-06-17 20:24:13 -0400

    Items associated with different lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -17,10 +17,10 @@ from django.conf.urls import include, url, patterns
 from lists import views
 
 urlpatterns = patterns('',
-	url(r'^$', 'lists.views.home_page', name='home'),
-	url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list',
-		name='view_list'
-	),
-	url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
-	# url(r'^admin/', include(admin.site.urls)),
+                       url(r'^$', 'lists.views.home_page', name='home'),
+                       url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list',
+                           name='view_list'
+                        ),
+                        url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
+                        # url(r'^admin/', include(admin.site.urls)),
 )
