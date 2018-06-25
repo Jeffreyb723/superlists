f9e298dc15bec615572df7a5ada173bf73bae9aa
Jeff Bupp <jbupp@DOMAIN.local>
2018-06-24 23:08:38 -0400

    Refactor list view to handle new item POSTs

--- lists/urls.py
+++ lists/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import url
-from lists.views import view_list, add_item, new_list
+from lists.views import view_list, new_list
 
 urlpatterns = [
     url(r'^(\d+)/$', view_list, name='view_list'),
-    url(r'^(\d+)/add_item$', add_item, name='add_item'),
     url(r'^new$', new_list, name='new_list'),
 ]
