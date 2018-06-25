7c31690d8279352371f7412d61581e654900fe82
Jeff Bupp <jbupp@DOMAIN.local>
2018-06-24 22:52:09 -0400

    Adjust new list view to do model validation

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,3 +1,4 @@
+from django.utils.html import escape
 from lists.models import Item, List
 from django.template.loader import render_to_string
 from django.core.urlresolvers import resolve
@@ -44,7 +45,7 @@ class ListViewTest(TestCase):
             response = self.client.get('/lists/%d/' % (correct_list.id,))
             self.assertEqual(response.context['list'], correct_list)
 
-class NewItemTest(TestCase):
+class NewListTest(TestCase):
     def test_can_save_a_POST_request_to_an_existing_list(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
@@ -69,3 +70,15 @@ class NewItemTest(TestCase):
         )
 
         self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
+
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+
+    def test_invalid_list_items_arent_saved(self):
+        self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
